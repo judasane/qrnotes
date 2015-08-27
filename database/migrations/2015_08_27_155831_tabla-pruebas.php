0bb1983ecdb1da4461951759046704873b7361cd
Juan Sanchez <judasane@gmail.com>
2015-08-27 16:03:16 +0000

    pruebas migration

---
+++ database/migrations/2015_08_27_155831_tabla-pruebas.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TablaPruebas extends Migration {
+
+	 public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('airline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('flights');
+    }
+
+}
