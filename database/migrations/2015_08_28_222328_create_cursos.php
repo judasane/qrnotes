880f0ebb88375b913dce599492376b522a6958c4
Juan Sanchez <judasane@gmail.com>
2015-08-28 22:52:30 +0000

    cartones arreglado

--- database/migrations/2015_08_28_222328_create_cursos.php
+++ database/migrations/2015_08_28_222328_create_cursos.php
@@ -12,7 +12,7 @@ class CreateCursos extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('cartones', function(Blueprint $table){
+		Schema::create('cursos', function(Blueprint $table){
 			$table->increments("id");
 			$table->string("nombre");
 			$table->string("codigo");
