6acb4e0172cf589d25525a8bdb9cd1baa2f03a61
Juan Sanchez <judasane@gmail.com>
2015-08-29 00:39:15 +0000

    cartones arreglado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Bienvenidos a Qrnotes</div>
+				<div class="title">Hola Monica</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
