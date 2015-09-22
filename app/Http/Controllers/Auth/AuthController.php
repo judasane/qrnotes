b15390d37461b52888e7f4709ff0cc671ddbb822
Juan_2 <Juan_2@Juan-PC>
2015-09-22 00:10:21 -0500

    Login y registro listos. probado en sftp y corriendo elriesgo de funcionar con esto

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,38 +1,97 @@
-<?php namespace App\Http\Controllers\Auth;
+<?php
+
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller {
+    /*
+      |--------------------------------------------------------------------------
+      | Registration & Login Controller
+      |--------------------------------------------------------------------------
+      |
+      | This controller handles the registration of new users, as well as the
+      | authentication of existing users. By default, this controller uses
+      | a simple trait to add these behaviors. Why don't you explore it?
+      |
+     */
 
-	/*
-	|--------------------------------------------------------------------------
-	| Registration & Login Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
+use AuthenticatesAndRegistersUsers;
 
-	use AuthenticatesAndRegistersUsers;
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Guard  $auth
+     * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+     * @return void
+     */
+    public function __construct(Guard $auth, Registrar $registrar) {
+        $this->auth = $auth;
+        $this->registrar = $registrar;
 
-	/**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
-	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
-	 * @return void
-	 */
-	public function __construct(Guard $auth, Registrar $registrar)
-	{
-		$this->auth = $auth;
-		$this->registrar = $registrar;
+        $this->middleware('guest', ['except' => 'getLogout']);
+    }
+    
+    public function getIndex(){
+         return redirect("auth/login");
+    }
+    public function postRegister(Request $request) {
+        $validator = $this->registrar->validator($request->all());
 
-		$this->middleware('guest', ['except' => 'getLogout']);
-	}
+        if ($validator->fails()) {
+            $this->throwValidationException(
+                    $request, $validator
+            );
+        }
+
+        $user = $this->registrar->create($request->all());
+
+        return "Se ha enviado un correo de verificación a $user->email";
+    }
+
+    public function getVerificar(Request $request) {
+
+        $code = $request->input("code");
+        if ($code == NULL || strlen($code) != 30) {
+            return "Has llegado acá por error";
+        } else {
+
+            $user = \App\User::where("password", $code)->first();
+            if ($user == null) {
+                return "Aleja tus manitas creativas de nosotros";
+            } else {
+                return view("auth.verificado", ["nombre" => $user->nombre, "id" => $user->id]);
+            }
+        }
+    }
+
+    /**
+     * Requiere id y password por post
+     * @param Request $request
+     */
+    public function postVerificar(Request $request) {
+        $validator = $this->validatorPass($request->all());
+        if ($validator->fails()) {
+            $this->throwValidationException(
+                    $request, $validator
+            );
+        }
+        $user = \App\User::where("id", $request->input("id"))->first();
+        $password = $request->input("password");
+        $user->password = bcrypt($password);
+        $user->save();
+        $this->auth->login($user);
+        return redirect($this->redirectPath());
+    }
+
+    public function validatorPass(array $data) {
+        return \Validator::make($data, [
+                    'password' => 'required|confirmed|min:6'
+        ]);
+    }
 
 }
--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -7,7 +7,8 @@ use Validator;
 use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
 
 class Registrar implements RegistrarContract {
-
+    private $data=[];
+    private $confirmation_code="";
     /**
      * Get a validator for an incoming registration request.
      *
@@ -21,7 +22,7 @@ class Registrar implements RegistrarContract {
                     'genero' => 'required|in:masculino,femenino',
                     'email' => 'required|email|max:255|unique:users',
                     'carrera' => 'required|max:255',
-                    'password' => 'required|confirmed|min:6',
+//                    'password' => 'required|confirmed|min:6',
                     'nacimiento'=>'required|date',
         ]);
     }
@@ -33,15 +34,24 @@ class Registrar implements RegistrarContract {
      * @return User
      */
     public function create(array $data) {
-        return User::create([
+        $this->data=$data;
+        $this->confirmation_code = str_random(30);
+        $user= User::create([
                     'nombre' => $data['nombre'],
                     'apellido' => $data['apellido'],
                     'genero' => $data['genero'],
                     'email' => $data['email'],
                     'carrera' => $data['carrera'],
                     'nacimiento' => $data['nacimiento'],
-                    'password' => bcrypt($data['password']),
+                    'password' => $this->confirmation_code,
         ]);
+        
+        \Mail::send("emails.verify",["nombre"=>$this->data["nombre"],"confirmation_code"=>$this->confirmation_code],function($message){
+        $message->to($this->data["email"],$this->data["nombre"]." ".$this->data["apellido"])->subject("Correo de verificación"); 
+        });
+        
+        return $user;
+        
     }
 
 }
