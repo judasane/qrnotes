922d9c6ff0cb92ac0840534129407915215b60a8
Juan Sanchez <judasane@gmail.com>
2015-09-26 11:08:44 +0000

    arreglo de problema de tabla que no existe queno permitía el deploy

--- database/migrations/2015_08_28_221835_create_packs.php
+++ database/migrations/2015_08_28_221835_create_packs.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCartones extends Migration {
+class CreatePacks extends Migration {
 
 	/**
 	 * Run the migrations.
