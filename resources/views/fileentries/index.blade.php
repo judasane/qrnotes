acf39b46f4c158c246106eaf580fb9191a3811e5
Juan Sanchez <judasane@gmail.com>
2015-09-26 13:35:51 +0000

    subida

---
+++ resources/views/fileentries/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+@section('title', 'Sube un archivo')
+@section('description','Aquí subir tus archivos');
+@section('content')
+ 
+    <form action="add" method="post" enctype="multipart/form-data">
+        <input type="file" name="filefield">
+        <input type="submit">
+    </form>
+ 
+ <h1> Pictures list</h1>
+ 
+ <div class="row">
+ 
+    <ul>
+ @foreach($entries as $entry)
+        <li>{{entry->filename}}</li>
+ @endforeach
+    </ul>
+ </div>
+ 
+@endsection
